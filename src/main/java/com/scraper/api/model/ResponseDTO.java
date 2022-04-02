93977a6ce8fdba0ccacb30e77b07d1dfa31c860a
sumitkumar1987 <102684490+sumitkumar1987@users.noreply.github.com>
2022-04-01 23:31:10 -0400

    code for web scrape

---
+++ src/main/java/com/scraper/api/model/ResponseDTO.java
@@ -0,0 +1,20 @@
+package com.scraper.api.model;
+
+public class ResponseDTO {
+
+    String title;
+    String url;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+}
